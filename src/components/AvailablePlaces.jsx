cc820f1be6e85ccdaa370bb8ebfa8c28efb159d5
Oksana Kormilina <keysb@mail.ru>
2024-01-29 18:01:24 +0400

    add async/await to http request

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -4,13 +4,25 @@ import Places from "./Places.jsx";
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
-  useEffect(() =>{
-  fetch("http://localhost:3000/places").then((response) => {
-    return response.json();
-  }).then((responseData) => {
-    setAvailablePlaces( responseData.places)
-  })}, [])
+  // useEffect(() => {
+  //   fetch("http://localhost:3000/places")
+  //     .then((response) => {
+  //       return response.json();
+  //     })
+  //     .then((responseData) => {
+  //       setAvailablePlaces(responseData.places);
+  //     });
+  // }, []);
 
+  // with async / await:
+  useEffect(() => {
+    async function fetchPlaces() {
+      const response = await fetch("http://localhost:3000/places");
+      const responseData = await response.json();
+      setAvailablePlaces(responseData.places);
+    }
+    fetchPlaces()
+  }, []);
 
   return (
     <Places

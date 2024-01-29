38e970671fcee87ee6b1166ab6fe6f8903e04e85
Oksana Kormilina <keysb@mail.ru>
2024-01-29 19:26:39 +0400

    Outsource fetching function to separate helper file

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import Places from "./Places.jsx";
 import Error from "./Error.jsx";
 import { sortPlacesByDistance } from "../loc.js";
+import { fetchAvailablePlaces } from "../http.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
@@ -23,16 +24,11 @@ export default function AvailablePlaces({ onSelectPlace }) {
     async function fetchPlaces() {
       setIsFetching(true);
       try {
-        const response = await fetch("http://localhost:3000/places");
-        const responseData = await response.json();
-        if (!response.ok) {
-          const error = new Error("Failed to fetch places");
-          throw error;
-        }
+     const places = await fetchAvailablePlaces()
 
         navigator.geolocation.getCurrentPosition((position) => {
          const sortedPlaces =  sortPlacesByDistance(
-            responseData.places,
+            places,
             position.coords.latitude,
             position.coords.longitude
           );
